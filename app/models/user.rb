8f08d4ac3e9f49634b4c6005a218139b4188c12a
aczapata <andreaczapata@gmail.com>
2015-10-01 16:48:00 -0500

    assets

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
-   has_attached_file :avatar, :styles => { :thumb => "32x32#" }, :default_url => "/images/default.png"
+  has_attached_file :avatar, :styles => { :thumb => "32x32" }, :default_url => "/images/default.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  validates :username, presence: true
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
