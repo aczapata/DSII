053b083b0c875e6dcb03b97b735424d2f8350045
root <melissaam@uninorte.edu.co>
2015-10-01 00:00:20 -0500

    Añadir username and picture

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+   has_attached_file :avatar, :styles => { :thumb => "32x32#" }, :default_url => "/images/default.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
