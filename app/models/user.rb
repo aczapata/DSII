202eeaea2e9120617a79510d47bf507edbad25d8
Andrea Zapata <andreaczapata@gmail.com>
2015-10-02 05:40:16 +0000

    Add profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
   has_attached_file :avatar, :styles => { :thumb => "32x32" }, :default_url => "/images/default.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   validates :username, presence: true
+  validates :username, :uniqueness => true
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
-  devise_for :users
+  devise_for :users, :path_prefix => 'd'
+  resources :users, :only => [:show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
