6c74ba6512ae83304764c0446e5fff2a67108a81
Andrea Zapata <andreaczapata@gmail.com>
2015-10-02 06:09:19 +0000

    Add panels profile

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   devise_for :users, :path_prefix => 'd'
   resources :users, :only => [:show]
+  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
