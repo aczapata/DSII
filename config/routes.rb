143b222e6fc68a485b574b9fba2391d20112d545
Andrea Zapata <andreaczapata@gmail.com>
2015-10-02 07:55:41 +0000

    Add Our team

--- config/routes.rb
+++ config/routes.rb
@@ -59,4 +59,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  
+  get 'aboutus' => 'static#about-us', as: :about
 end
